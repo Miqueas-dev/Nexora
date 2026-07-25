72beaee4f3efb4e6f7c23a9c98094deb6d5f625d
alex-git-dev-25 <alexapril17@gmail.com>
2026-07-25 16:46:28 -0500

    Creacion de Metodos en MarcaService

--- src/main/java/pe/edu/cibertec/nexora/service/MarcaService.java
+++ src/main/java/pe/edu/cibertec/nexora/service/MarcaService.java
@@ -6,5 +6,16 @@ import pe.edu.cibertec.nexora.entity.Marca;
 
 public interface MarcaService {
 	
+	//Metodo para listar Marcas
+	List<Marca> listar();
+	//Metodo para registrar nueva marca
+	Marca guardar(Marca marca);
+	//Metodo usado en el proceso de actualizar marca
+	Marca buscarPorId(Integer id);
+	//Metodo para eliminar marca
+	void eliminar(Integer id);
+	
+	
+	//Metodo para buscar Marca por texto ingresado
 	List<Marca> buscarPorTextoIngresado(String marca_textoIngresado);
 }
