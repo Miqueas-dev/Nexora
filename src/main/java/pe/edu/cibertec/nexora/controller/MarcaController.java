f69a9eb2ba0b35fbb4b7a277b550de1d1f415adf
alex-git-dev-25 <alexapril17@gmail.com>
2026-07-25 20:26:26 -0500

    feat: agrega endpoint GET /marcas/nuevo

--- src/main/java/pe/edu/cibertec/nexora/controller/MarcaController.java
+++ src/main/java/pe/edu/cibertec/nexora/controller/MarcaController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import pe.edu.cibertec.nexora.entity.Marca;
 import pe.edu.cibertec.nexora.service.MarcaService;
 
 @Controller
@@ -23,6 +27,13 @@ public class MarcaController {
 		model.addAttribute("texto",texto);
 		return "/marcas/lista";
 	}
+	
+	@GetMapping("/nuevo")
+	public String nuevaMarca(Model model) {
+		model.addAttribute("marca", new Marca());
+		return "marcas/formulario";
+	}
+	
 	
 	
 }
