24d34e5efe1213211d4456cd4fdd734405caee9f
alex-git-dev-25 <alexapril17@gmail.com>
2026-08-10 23:31:49 -0500

    feat: agrega endpoint GET /api/estados/{id}

--- src/main/java/pe/edu/cibertec/nexora/controller/EstadoRestController.java
+++ src/main/java/pe/edu/cibertec/nexora/controller/EstadoRestController.java
@@ -2,12 +2,15 @@ package pe.edu.cibertec.nexora.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pe.edu.cibertec.nexora.entity.Estado;
+import pe.edu.cibertec.nexora.entity.Marca;
 import pe.edu.cibertec.nexora.service.EstadoService;
 
 @RestController
@@ -20,14 +23,21 @@ public class EstadoRestController {
 		this.estadoService=estadoService;
 	}
 	
-	//Endpoint: GET /api/marcas
-		@GetMapping
-		public ResponseEntity<List<Estado>> listarEstados(){
-			List<Estado> estados=estadoService.listar();
-			return ResponseEntity.ok(estados);
-		}
-	
-	
+	//Endpoint: GET /api/estados
+	@GetMapping
+	public ResponseEntity<List<Estado>> listarEstados(){
+		List<Estado> estados=estadoService.listar();
+		return ResponseEntity.ok(estados);
+	}
+
+	//Endpoint: GET /api/estados/{id}
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarEstado(@PathVariable Integer id){
+		Estado estado = estadoService.buscarPorId(id);
+		if (estado==null)
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("[Proceso de busqueda] El estado con ID "+id+" no existe.");
+		return ResponseEntity.ok(estado);
+	}
 	
 	
 }
